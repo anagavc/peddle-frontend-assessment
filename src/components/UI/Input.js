f92cc60c2a719c2160da727872ba2815063a4707
Anaga Victor <anagavc@gmail.com>
2022-07-18 09:45:05 +0100

    Added the favicon and fixed the site manifest

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -5,7 +5,7 @@ const Input = ({ placeholder, name, id, register, errors }) => {
         placeholder={placeholder}
         id={id}
         name={name}
-        className="bg-transparent rounded w-full placeholder:text-[#707070] text-[#707070] border-b border-b-[#707070] py-2 px-4 font-heading text-base  focus:outline-none  focus:border-[#707070] transition focus:ring-[#707070] focus:ring-1 duration-300"
+        className="bg-transparent  w-full placeholder:text-[#707070] text-[#707070] border-b border-b-[#707070] py-2 px-4 font-heading text-base  focus:outline-none  focus:border-[#707070] transition focus:ring-[#707070] focus:ring-1 duration-300"
         {...register(name, {
           required: `${placeholder} is required`,
           minLength: {
